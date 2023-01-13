8ffa2ebf2a850b52d41a375ac582171dabbb3479
LUCAS PERRONI MENDES PEREIRA <lucas.pmpereira@senacsp.edu.br>
2023-01-12 22:05:55 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCEP/ConsultaCEP/Views/NewItemPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ConsultaCEP.Models;
+using ConsultaCEP.Services;
+using ConsultaCEP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConsultaCEP.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+
+        private void MetodoPesquisaCEP(object sender, EventArgs e)
+        {
+            PesquisarCEP pesquisaCEP = new PesquisarCEP();
+
+            ViaCEP oViacep = new ViaCEP();
+            oViacep = pesquisaCEP.Consulta(TextCep1.Text);
+
+            lbl_logradouro.Text = oViacep.logradouro;
+
+        }
+    }
+
+}
